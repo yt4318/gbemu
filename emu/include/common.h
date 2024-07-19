6da94f969ef23e3369f0ec5fe1d53cec6726c9ab
yt <tsujichanta3825@gmail.com>
2024-07-19 14:37:11 +0900

    Implement basic CPU emulation logic

--- emu/include/common.h
+++ emu/include/common.h
@@ -12,8 +12,10 @@ typedef uint64_t u64;
 
 #define BIT(a, n) ((a & (1 << n)) ? 1 : 0)
 
-#define BIT_SET(a, n, on) (on ? (a) |= (1 << n) : (a) &= ~(1 << n))
+#define BIT_SET(a, n, on) { if (on) a |= (1 << n); else a &= ~(1 << n);}
 
 #define BETWEEN(a, b, c) ((a >= b) && (a <= c))
 
 void delay(u32 ms);
+
+#define NO_IMPL {fprintf(stderr, "NOT YET IMPLEMENT\n");exit(-5);}
