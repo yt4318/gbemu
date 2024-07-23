7ba8d0982360dcea4556e2bab2d4ca2742290c42
yt <tsujichanta3825@gmail.com>
2024-07-23 22:47:07 +0900

    Implement LD instruction

--- emu/lib/bus.c
+++ emu/lib/bus.c
@@ -1,10 +1,12 @@
 #include <bus.h>
+#include <cart.h>
 
 u8 bus_read(u16 address) {
     if(address < 0x8000) {
         return cart_read(address);
     }
-    NO_IMPL
+    
+    printf("UNSUPPORTED bus_read(%04X)\n", address);
 }
 
 void bus_write(u16 address, u8 value){
@@ -12,5 +14,16 @@ void bus_write(u16 address, u8 value){
         cart_write(address, value);
         return;
     }
-    NO_IMPL
+    printf("UNSUPPORTED bus_write(%04X)\n", address);
+}
+
+u16 bus_read16(u16 address) {
+    u16 lo = bus_read(address);
+    u16 hi = bus_read(address + 1);
+    return lo | (hi << 8);
+}
+
+void bus_write16(u16 address, u16 value) {
+    bus_write(address + 1, (value >> 8) & 0xFF);
+    bus_write(address, value & 0xFF);
 }
